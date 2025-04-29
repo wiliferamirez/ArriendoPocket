a911c7c5a832bc52c904d466de2637847f16530d
WilliamRamirez <wframirez98@gmail.com>
2025-04-28 17:41:13 -0700

    Crear Login VIewModel

---
+++ Areas/Identity/Pages/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArriendoPocket.Areas.Identity.Pages
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage ="Campo Obligatorio")]
+        [EmailAddress(ErrorMessage = "Correo Invalido")]
+        public string Correo { get; set; }
+
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        [DataType(DataType.Password)]
+        public string Contrasena { get; set; }
+
+        [Display(Name = "Recordarme?")]
+        public bool RememberMe { get; set; }
+    }
+}
