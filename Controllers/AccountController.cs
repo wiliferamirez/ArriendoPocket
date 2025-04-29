5e8d708dd4bf613761fe3f8df1141b3fd136ef3a
WilliamRamirez <wframirez98@gmail.com>
2025-04-28 20:09:16 -0700

    Agregar metodo post para registarar ususario

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ArriendoPocket.Areas.Identity.Pages;
+using ArriendoPocket.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ArriendoPocket.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly SignInManager<Arrendatario> signInManager;
+        private readonly UserManager<Arrendatario> userManager;
         public IActionResult Login()
         {
             return View();
@@ -11,6 +16,36 @@ namespace ArriendoPocket.Controllers
         public IActionResult Register()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Arrendatario arrendatario = new Arrendatario
+                {
+                    CedulaArrendatario = model.CedulaArrendatario,
+                    NombreArrendatario = model.NombreArrendatario,
+                    ApellidoArrendatario = model.ApellidoArrendatario,
+                    CorreoArrendatario = model.CorreoArrendatario,
+                    TelefonoArrendatario = model.TelefonoArrendatario,
+                    FechaNacimientoArrendatario = model.FechaNacimientoArrendatario
+                };
+                var result = await userManager.CreateAsync(arrendatario, model.Contrasena);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login", "Account");
+                }
+                else
+                {
+                    foreach(var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                    return View(model);
+                }
+            }
+            return View(model);
         }
     }
 }
