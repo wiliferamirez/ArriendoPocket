5325ab5d45db87284fd379771ee591063a31acb7
WilliamRamirez <wframirez98@gmail.com>
2025-04-28 18:17:40 -0700

    Agregar controlador par Cuenta incluyendo Login y Registro

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ArriendoPocket.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
