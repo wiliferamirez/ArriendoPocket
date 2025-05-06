fc008550ef38b9bdb7b3f816dcd6557f6c09e13d
WilliamRamirez <wframirez98@gmail.com>
2025-05-06 03:13:37 -0700

    Agregar Propieades a navbar

--- Controllers/PropiedadesController.cs
+++ Controllers/PropiedadesController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ArriendoPocket.Data;
 using ArriendoPocket.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ArriendoPocket.Controllers
 {
@@ -20,6 +21,7 @@ namespace ArriendoPocket.Controllers
         }
 
         // GET: Propiedades
+        [Authorize]
         public async Task<IActionResult> Index()
         {
             var applicationDbContext = _context.Propiedades.Include(p => p.Propietario);
